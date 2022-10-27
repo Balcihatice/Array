9fb5d55f175c4e8bcb198d237f31afe648f4f0ea
Balcihatice <aciceaybike@gmail.com>
2022-10-27 17:10:01 +0300

    enBuyuk enKucuk

---
+++ src/main/java/arrays/Arrays_09.java
@@ -0,0 +1,27 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class Arrays_09 {
+
+     /*
+    Verilen bir arrayde en kucuk ve en buyuk elemanlari yazdiran bir method olusturunuz
+     */
+
+
+    public static void enBuyukEnKucukElemanYazdir(int[] arr) {
+
+        Arrays.sort(arr);
+
+        System.out.println("En Buyuk Eleman :" + arr[arr.length - 1]);
+
+        System.out.println("En Kucuk Eleman :" + arr[0]);
+    }
+
+    public static void main(String[] args) {
+
+        int[] arr = {23, 76, 45, 90, 11};
+
+        enBuyukEnKucukElemanYazdir(arr);
+    }
+}
